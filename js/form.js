2f2f27f200324b2159d4e22892003255295d41b3
julia-marta <marta237@yandex.ru>
2020-07-09 14:26:21 +0300

    Выполняет задание Личный проект: модуляция (часть 2)

--- js/form.js
+++ js/form.js
@@ -1,16 +1,6 @@
 'use strict';
 
 (function () {
-  var Filter = {
-    blur: {
-      MIN: 0,
-      MAX: 3
-    },
-    brightness: {
-      MIN: 1,
-      MAX: 3
-    }
-  };
 
   var Scale = {
     MIN: 25,
@@ -24,9 +14,6 @@
   var image = form.querySelector('.img-upload__preview img');
   var filters = form.querySelector('.effects');
   var slider = form.querySelector('.effect-level');
-  var sliderToggle = slider.querySelector('.effect-level__pin');
-  var sliderBar = slider.querySelector('.effect-level__line');
-  var effectLevelInput = slider.querySelector('input[name=effect-level]');
   var decreaseControl = form.querySelector('.scale__control--smaller');
   var increaseControl = form.querySelector('.scale__control--bigger');
   var scaleLevelInput = form.querySelector('input[name=scale]');
@@ -59,12 +46,12 @@
 
   uploadField.addEventListener('change', showForm);
 
-  // добавление фильтра на фотографию
+  // переключение фильтров
 
-  var changeFilter = function (evt) {
+  var onFilterToggle = function (evt) {
     image.className = '';
     image.style.filter = '';
-    effectLevelInput.value = 100 + '%';
+    window.slider.reset();
     image.classList.add('effects__preview--' + evt.target.value);
     if (image.className === 'effects__preview--none') {
       slider.classList.add('hidden');
@@ -73,36 +60,7 @@
     }
   };
 
-  filters.addEventListener('change', changeFilter);
-
-  // изменение насыщенности эффекта
-
-  var changeEffectLevel = function () {
-    var togglePosition = sliderToggle.offsetLeft + (sliderToggle.offsetWidth / 2);
-    var effectLevelPercent = Math.round(togglePosition * 100 / sliderBar.offsetWidth);
-    var effectLevelDecimal = (effectLevelPercent / 100).toFixed(1);
-    effectLevelInput.value = effectLevelPercent + '%';
-
-    switch (image.className) {
-      case ('effects__preview--chrome'):
-        image.style.filter = 'grayscale(' + effectLevelDecimal + ')';
-        break;
-      case ('effects__preview--sepia'):
-        image.style.filter = 'sepia(' + effectLevelDecimal + ')';
-        break;
-      case ('effects__preview--marvin'):
-        image.style.filter = 'invert(' + effectLevelPercent + '%)';
-        break;
-      case ('effects__preview--phobos'):
-        image.style.filter = 'blur(' + effectLevelDecimal * Filter.blur.MAX + 'px)';
-        break;
-      case ('effects__preview--heat'):
-        image.style.filter = (effectLevelDecimal * Filter.brightness.MAX <= Filter.brightness.MIN) ? 'brightness(' + Filter.brightness.MIN + ')' : 'brightness(' + effectLevelDecimal * Filter.brightness.MAX + ')';
-        break;
-    }
-  };
-
-  sliderToggle.addEventListener('mouseup', changeEffectLevel);
+  filters.addEventListener('change', onFilterToggle);
 
   // изменение масштаба фотографии
 
@@ -135,6 +93,8 @@
   increaseControl.addEventListener('click', onIncreaseControlClick);
 
   window.form = {
-    dialog: form
+    dialog: form,
+    slider: slider,
+    image: image
   };
 })();
